4a63a16e111d787525569dd53d91e8a15c5ae762
waiwuo1412 <61792863+waiwuo1412@users.noreply.github.com>
2022-06-29 21:45:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ import subprocess
 import yt_dlp
 
 TARGET = ''  # 设置url获取路径
-PREFIX = ''
+PREFIX = 'https://www.youtube.com/watch?v='
 SAVE_PATH = ''  # 设置保存路径
-# 某些视频可能需要登录才可以查看
-USER_NAME = ''  # 输入youtube用户名
-PASSWORD = ''  # 输入youtube密码
-THREAD_NUM = 8  # 设置爬取的线程数量
+# 某些视频可能需要登录才可以查看[选填]
+USER_NAME = null  # 设置youtube用户名,可以不填
+PASSWORD = null  #设置youtube密码，可以不填
+THREAD_NUM = 8  # 设置爬取的线程数量，可以不填
 
 # 配置logging参数
 logging.basicConfig(level=logging.ERROR,
@@ -49,11 +49,16 @@ class MyLogger(object):
 
 
 ydl_opts = {
-    'quiet': True,
+    'quiet': True,  # 
     'username': USER_NAME,
     'password': PASSWORD,
     'logger': MyLogger(),
     'retries': 50
+    # 'postprocessors': [{
+        #     'key': 'FFmpegExtractAudio',
+        #     'preferredcodec': suffix,
+        #     'preferredquality': '192',
+        # }],
 }
 
 
